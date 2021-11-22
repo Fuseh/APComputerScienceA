016ef29b0d922add6696c7dcb3d1ae084ab8866c
Fuseh <pg13ftw@gmail.com>
2021-11-22 09:30:30 -0500

    Initial Commit

---
+++ src/Ch7Practice.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Ch7Practice{
+    public static void main(String[]args){
+        int[] values = new int[10];
+        for (int i = 0; i < values.length; i++)
+
+        {
+
+            if (i > 0)
+
+            {
+
+                System.out.print(" | ");
+
+            }
+
+            System.out.print(values[i]);
+
+        }
+    }
+}
+
